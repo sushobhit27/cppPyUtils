a84c356e513c154418eb12cbe821f37f11f1fd87
Sushobhit27 <sushobhitsolanki@gmail.com>
2019-08-22 21:52:56 +0530

    Add is_in function for both sequential and associate containers. It simulates behaviour of in operator of python.

--- src/utils.hpp
+++ src/utils.hpp
@@ -34,3 +34,48 @@ std::string swapcase(std::string str);
 std::string zfill(const std::string &str, size_t width);
 bool startswith(const std::string &substr, const std::string &str, size_t beg = 0, size_t end = std::string::npos);
 bool endswith(const std::string &substr, const std::string &str, size_t beg = 0, size_t end = std::string::npos);
+
+
+template <typename T,
+    typename Container>
+bool is_in(const T &val, 
+    Container& container,
+    typename std::enable_if<std::is_same<T, typename Container::value_type>::value, T>::type* = nullptr)
+{
+    return std::find(container.begin(), container.end(), val) != container.end();
+}
+
+template <typename T,
+    typename Container,
+    typename UnaryPredicate>
+bool is_in(const T &val, 
+    Container& container,
+    UnaryPredicate predicate,
+    typename std::enable_if<std::is_same<T, typename Container::value_type>::value, T>::type* = nullptr)
+{
+    return std::find_if(container.begin(), container.end(), predicate) != container.end();
+}
+
+template <typename T,
+    typename Container>
+bool is_in(const T &val, Container& container,
+    typename std::enable_if<std::is_same<T, typename Container::key_type>::value, T>::type* = nullptr)
+{
+    return container.find(val) != container.end();
+}
+
+template <typename T,
+    typename Container,
+    typename UnaryPredicate>
+bool is_in(const T &val, 
+    Container& container,
+    UnaryPredicate predicate,
+    typename std::enable_if<std::is_same<T, typename Container::key_type>::value, T>::type* = nullptr)
+{
+    for (const auto& pair : container)
+    {
+        if (predicate(pair.first))
+            return true;
+    }
+    return false;
+}
